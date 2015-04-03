c42fbd926c5fb5dbe3eae2d229407dab7bce000e
szabivan <szabolcs.ivan@gmail.com>
2015-04-03 21:17:22 +0200

    stage border and a clause

--- inputres/resolver.js
+++ inputres/resolver.js
@@ -1,6 +1,15 @@
 var stage;
 
-function Literal( color, polarity, innerRadius, outerRadius ){
+/** MAGIC CONSTANTS **/
+var round = 5; // rounded rectangle's corner
+var Width = 400;
+var Height = 400;
+
+function getColor( i ){
+	return ["red", "green" "blue", "black"][i];
+}
+
+function Literal( color, polarity, innerRadius, outerRadius, x, y ){
   this.color = color;
   this.shape = new createjs.Shape();
   if( polarity > 0 ){
@@ -8,15 +17,42 @@ function Literal( color, polarity, innerRadius, outerRadius ){
   }else{
     this.shape.graphics.beginFill( color ).drawCircle( 0, 0, innerRadius + 1 );
   }
-  this.shape.x = this.shape.y = 0;
+  this.shape.x = x;
+  this.shape.y = y;
+}
+
+function Clause( arr, radius ){
+  this.size = 0;
+  this.container = new createjs.Container();
+  this.shape = new createjs.Shape();
+  this.shape.graphics.beginStroke("black").drawCircle( 0, 0, radius );
+  this.container.addChild( this.shape );
+  this.arr = arr.slice();
+  var tmpArr = [];
+  for( var i = 0; i < arr.length; i++ ) if( arr[i] ) { this.size++; tmpArr.push( (i+1)*arr[i] ); }
+  if( size == 1 ){
+    this.container.addChild( new Literal( getColor( Math.abs(tmpArr[0]) - 1 ), tmpArr[0], radius*1/2, radius*3/4, 0, radius / 4 ) );
+  }else if( size == 0 ){
+  }else{
+	  //TODO: nagyobbakat is kezelni
+  }  
+}
+Clause.prototype.addToStage = function(){
+	stage.addChild( this.container );
+}
+
+function createBorders(){
+	var shape = new createjs.Shape();
+	shape.graphics.beginStroke("black").drawRoundedRectangle(0,0,Width,Height,round);
+	stage.addChild( shape );
 }
 
 function init(){
 	stage = new createjs.Stage("gameCanvas");
+	createBorders();
 	
-	var l = new Literal( "green", 1, 10, 20 );
-	l.shape.x = l.shape.y = 100;
-	stage.addChild( l.shape );
+    var c = new Clause( [0,1,0], 30 );
+	c.addToStage();
 	
 	stage.update();
 }
