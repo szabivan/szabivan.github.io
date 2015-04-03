8d88ab42e3f9424534221dfcebf0771e5765b817
szabivan <szabolcs.ivan@gmail.com>
2015-04-03 20:54:57 +0200

    always check the specs

--- inputres/resolver.js
+++ inputres/resolver.js
@@ -4,9 +4,9 @@ function Literal( color, polarity, innerRadius, outerRadius ){
   this.color = color;
   this.shape = new createjs.Shape();
   if( polarity > 0 ){
-    this.shape.graphics.beginFill( color ).drawCircle( outerRadius ).beginFill( "white" ).drawCircle( innerRadius );
+    this.shape.graphics.beginFill( color ).drawCircle( 0, 0, outerRadius ).beginFill( "white" ).drawCircle( 0, 0, innerRadius );
   }else{
-    this.shape.graphics.beginFill( color ).drawCircle( innerRadius + 1 );
+    this.shape.graphics.beginFill( color ).drawCircle( 0, 0, innerRadius + 1 );
   }
   this.shape.x = this.shape.y = 0;
 }
